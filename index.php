aa37f3fe0806f22c7a74421f36dfd19d779a449d
sajadmomeni <sajad79sp79@gmail.com>
2021-04-22 18:01:50 +0430

    Section_1

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $arr = [
+        [
+            "name"=>"Ali",
+            "age"=>25,
+            "city"=>"Tehran",
+        ],
+        [
+            "name"=>"Mehdi",
+            "age"=>25,
+            "city"=>"Tehran",
+        ],
+        [
+            "name"=>"Ali",
+            "age"=>25,
+            "city"=>"Tehran",
+        ],
+    ];
+
+    var_dump($arr[0]["name"]);
+?>
