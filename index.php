018cea4fabb64875ed508a2a061148c88d48ac31
sajadmomeni <sajad79sp79@gmail.com>
2021-04-25 22:19:39 +0430

    Section_2

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <?php
 
-    $arr = [
-        [
-            "name"=>"Ali",
-            "age"=>25,
-            "city"=>"Tehran",
-        ],
-        [
-            "name"=>"Mehdi",
-            "age"=>25,
-            "city"=>"Tehran",
-        ],
-        [
-            "name"=>"Ali",
-            "age"=>25,
-            "city"=>"Tehran",
-        ],
-    ];
+    $arr = [8,2,9,4,9,6,7,5,2,8,8,8,8,9,9,2,4];
 
-    var_dump($arr[0]["name"]);
+    $length = count($arr);
+    for ($i=0; $i < $length; $i++){ 
+        $repeat = false;
+        for ($j=$i+1; $j < $length; $j++) { 
+            if($arr[$i] == $arr[$j]){
+                $repeat = true;
+            }
+        }
+        if(!$repeat)
+            echo $arr[$i];
+        echo "<br>";
+    }
+    /*
+    if(isset($_POST['submit']))
+    {
+        if($_POST['username'] == "admin" && $_POST['password'] == "admin"){
+            echo "Access";
+        }
+        else{
+            echo "Access denied";
+        }
+    }*/
 ?>
+<!--
+<form action="" method="POST">
+    <input type="text" name="username" placeholder="Username:" required>
+    <input type="text" name="password" placeholder="Password:" required>
+
+    <button name="submit">Login</button>
+</form> -->
