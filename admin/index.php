d9434c6fac4601c6839f1b80807faab9bac01ae0
sajadmomeni <sajad79sp79@gmail.com>
2021-05-08 23:29:25 +0430

    Extra 50min

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf8">
+		<title>Dashboard - Admin</title>
+		<link rel="stylesheet" href="css/style.css" >
+	</head>
+
+	<body>
+		
+	</body>
+</html>
